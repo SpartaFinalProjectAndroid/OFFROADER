39c8e1279ebb885fec8b36ef5ed7106434f1b289
Box-o <jeffmailkr@gmail.com>
2024-02-26 04:52:43 +0900

    add : multiViewItem, implementation(nav frag,viewpager2) / feat : link navigationBar and multiViewRecyclerView

--- app/src/main/java/com/mit/offroader/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/mit/offroader/ui/fragment/home/HomeFragment.kt
@@ -1,32 +1,56 @@
 package com.mit.offroader.ui.fragment.home
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.AlertDialog
+import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mit.offroader.R
+import com.mit.offroader.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var myPageAdapter: HomeMultiViewTypeAdapter
+    var uiData: List<HomeUiData> = listOf()
 
-    private lateinit var viewModel: HomeViewModel
+    private val homeViewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        myPageAdapter = HomeMultiViewTypeAdapter(requireContext())
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        uiData = listOf(
+            HomeUiData.First,
+            HomeUiData.Second
+        )
+
+        binding.rvHome.adapter = myPageAdapter
+        binding.rvHome.layoutManager = LinearLayoutManager(requireContext())
+
+        myPageAdapter.submitList(uiData.toList())
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
