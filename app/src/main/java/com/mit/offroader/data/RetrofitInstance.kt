22f21da71868e754dd81aa15f6d2b1eb261fc6a4
Euny Kim <euny4535@gmail.com>
2024-02-27 23:48:21 +0900

    add : 챗지피티 api 연결

--- app/src/main/java/com/mit/offroader/data/RetrofitInstance.kt
+++ app/src/main/java/com/mit/offroader/data/RetrofitInstance.kt
@@ -1,5 +1,6 @@
 package com.mit.offroader.data
 
+import com.mit.offroader.data.api.AiApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -12,6 +13,8 @@ object RetrofitInstance {
     // 네이버 지도 API:
 
     // 챗GPT API:
+    private const val AI_BASE_URL = "https://api.openai.com/v1/"
+    val aiApi: AiApi by lazy { retrofit(AI_BASE_URL).create(AiApi::class.java) }
 
     // VWORLD API:
 
--- app/src/main/java/com/mit/offroader/data/api/AiApi.kt
+++ app/src/main/java/com/mit/offroader/data/api/AiApi.kt
@@ -1,4 +1,18 @@
 package com.mit.offroader.data.api
 
-class AiApi {
+
+import com.mit.offroader.data.model.ai.AiRequest
+import com.mit.offroader.data.model.ai.AiResponse
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+private const val AI_API_KEY = "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"
+const val AI_MODEL = "gpt-3.5-turbo"
+
+interface AiApi {
+    @Headers("Authorization: Bearer $AI_API_KEY")
+    @POST("chat/completions")
+
+    suspend fun createChatCompletion(@Body body: AiRequest) : AiResponse
 }
