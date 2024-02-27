e4ea4982fe8cc4703ad1047a01f5492761f3e93d
Euny Kim <euny4535@gmail.com>
2024-02-28 02:24:03 +0900

    bug : api 숨기기 시도 (챗지피티 api를 담은 채로 리포지토리에 푸시를 하면 자동으로 api가 비활성화 되어서 api를 무조건 숨겨줘야한다.

--- app/src/main/java/com/mit/offroader/data/api/AiApi.kt
+++ app/src/main/java/com/mit/offroader/data/api/AiApi.kt
@@ -7,8 +7,10 @@ import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
-private const val AI_API_KEY = "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"
+//private val aiApiKey : String = System.getenv("OPENAI_API_KEY") ?: "default_value"
 const val AI_MODEL = "gpt-3.5-turbo"
+
+private const val AI_API_KEY = ""
 
 interface AiApi {
     @Headers("Authorization: Bearer $AI_API_KEY")
