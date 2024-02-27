f206009df01c747884692954c819100bf3e53f8a
Euny Kim <euny4535@gmail.com>
2024-02-28 01:12:46 +0900

    feat : chatgpt api 연동 확인

--- app/src/main/java/com/mit/offroader/ui/fragment/chatbot/ChatBotFragment.kt
+++ app/src/main/java/com/mit/offroader/ui/fragment/chatbot/ChatBotFragment.kt
@@ -2,10 +2,12 @@ package com.mit.offroader.ui.fragment.chatbot
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
@@ -41,6 +43,9 @@ class ChatBotFragment : Fragment() {
     }
 
     private fun initObserver() {
+        chatBotViewModel.chatBotUiState.observe(viewLifecycleOwner) {
+            Log.d("CHATGPT CHATBOT", "^^ ${it.response}")
+        }
 
     }
 
@@ -53,8 +58,28 @@ class ChatBotFragment : Fragment() {
     프래그먼트를 최초 실행했을 때 실행 되는 함수를 모아놓은 함수
      **/
     private fun initView() {
-        setBotServiceProvider()            // 채팅 봇 "스피너"를 셋팅해주는 함수
-        setBot()                           // 스피너를 작동 (하이키/봉봉이) 을 셋팅해주는 함수
+
+        // 채팅 봇 "스피너"를 셋팅해주는 함수
+        setBotServiceProvider()
+
+        // 스피너를 작동 (하이키/봉봉이) 을 셋팅해주는 함수
+        setBot()
+        // 봇을 셋팅하면 봇에 따라서 챗을 셋팅해 줄 수 있도록 setChat() 함수는 setBot()함수 안에 추가해둠.
+
+        // EditText 검색 셋팅 함수
+        setSearch()
+    }
+
+    // 에딧 텍스트에서 챗봇 검색 실행 코드.
+    private fun setSearch() {
+        binding.etAsk.setOnEditorActionListener { textView, actionId, keyEvent ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                chatBotViewModel.onSearch(textView.toString())
+                handled = true
+            }
+            handled
+        }
     }
 
     // 채팅 봇 "스피너"를 셋팅해주는 함수
@@ -80,18 +105,20 @@ class ChatBotFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-                chatBotViewModel.setBotSpinner()
-                when (position) {
-                    0 -> {
-                        binding.ivBot.setImageResource(R.drawable.ic_hikey)
-                        // TODO : 채팅 화면 전환하기 (+ MVVM 모델 구조로 바꾸어야함)
-                    }
-                    1 -> {
-                        binding.ivBot.setImageResource(R.drawable.ic_bongbong)
-                        // TODO : 채팅 화면 전환하기 (+ MVVM 모델 구조로 바꾸어야함)
-                    }
-                }
-                // 로직 부분 구현하기
+                chatBotViewModel.setBotSpinner(position)
+//                when (position) {
+//                    0 -> {
+//                        binding.ivBot.setImageResource(R.drawable.ic_hikey)
+//                        // TODO : 채팅 화면 전환하기 (+ MVVM 모델 구조로 바꾸어야함)
+//                        setChat(position)
+//                    }
+//                    1 -> {
+//                        binding.ivBot.setImageResource(R.drawable.ic_bongbong)
+//                        // TODO : 채팅 화면 전환하기 (+ MVVM 모델 구조로 바꾸어야함)
+//                        setChat(position)
+//                    }
+//                }
+//                // 로직 부분 구현하기
 
             }
 
@@ -99,8 +126,23 @@ class ChatBotFragment : Fragment() {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 binding.ivBot.setImageResource(R.drawable.ic_bongbong)
                 // TODO : 채팅 화면 셋팅 ? sharedpref 또는 Room DB에 저장되어 있는 채팅 내역 가져오기
+                setChat(1)
             }
 
+        }
+
+    }
+
+    // 채팅 창을 셋팅해주는 함수
+    private fun setChat(bot: Int) { // 정수형 매개변수 bot 이 0이면 봉봉이 1이면 하이키
+        when (bot) {
+            0 -> {
+
+            }
+
+            1 -> {
+
+            }
         }
 
     }
