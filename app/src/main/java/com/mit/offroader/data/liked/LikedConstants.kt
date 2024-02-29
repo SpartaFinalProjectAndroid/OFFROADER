c8d173a49232bce9754aad18057e87cfc08aafd8
kiwon kim <rlarldnjs17@gmail.com>
2024-02-29 20:45:11 +0900

    feat: 자동 스크롤

---
+++ app/src/main/java/com/mit/offroader/data/liked/LikedConstants.kt
@@ -0,0 +1,6 @@
+package com.mit.offroader.data.liked
+
+object LikedConstants {
+    const val LIKED_PREFS_NAME = "com.mit.offroader.data.liked.prefs"
+    const val LIKED_PREF_KEY = "LIKED_PREF"
+}
--- app/src/main/java/com/mit/offroader/ui/activity/sandetail/SanDetailActivity.kt
+++ app/src/main/java/com/mit/offroader/ui/activity/sandetail/SanDetailActivity.kt
@@ -2,7 +2,10 @@ package com.mit.offroader.ui.activity.sandetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.viewModels
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.mit.offroader.R
 import com.mit.offroader.databinding.ActivityMainBinding
 import com.mit.offroader.databinding.ActivitySanDetailBinding
@@ -11,6 +14,12 @@ import com.mit.offroader.ui.activity.main.MainViewModel
 class SanDetailActivity : AppCompatActivity() {
     private var _binding: ActivitySanDetailBinding? = null
     private val binding get() = _binding!!
+    private val imageList = mutableListOf<SanDetailImageData>()
+    private val slideImageHandler: Handler = Handler()
+    private val slideImageRunnable =
+        Runnable { binding.vpMountain.currentItem = binding.vpMountain.currentItem + 1 }
+
+    private lateinit var imageAdapter: SanImageAdapter
     private val sanDetailViewModel by viewModels<SanDetailViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,32 +29,51 @@ class SanDetailActivity : AppCompatActivity() {
         initImage()
         initBookMarkButton()
 
-
         initBackButton()
     }
 
-    private fun initImage() {
-        imageAdapter = SanImageAdapter(mItems = List<SanDetailImageData>)
+    override fun onResume() {
+        super.onResume()
+        slideImageHandler.postDelayed(slideImageRunnable, 5000)
     }
 
+    override fun onPause() {
+        super.onPause()
+        slideImageHandler.removeCallbacks(slideImageRunnable)
+    }
+
+    // 자동 스크롤되는 ViewPager2 이미지
+    private fun initImage() {
+        imageAdapter = SanImageAdapter(imageList)
+        binding.vpMountain.adapter = imageAdapter
+        binding.vpMountain.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        binding.vpMountain.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                slideImageHandler.removeCallbacks(slideImageRunnable)
+                slideImageHandler.postDelayed(slideImageRunnable, 5000)
+            }
+        })
+    }
+
+    // 좋아요 기능
     private fun initBookMarkButton() {
         val bookmarkOn = resources.getDrawable(R.drawable.ic_bookmark_on)
         val bookmarkOff = resources.getDrawable(R.drawable.ic_bookmark_off)
 
 
-
-        binding.ivBookmark.setOnClickListener {
-            binding.ivBookmark.setImageResource(
-                if(isLiked) bookmarkOn else bookmarkOff
-            )
-        }
+//        binding.ivBookmark.setOnClickListener {
+//            binding.ivBookmark.setImageResource(
+//                if(isLiked) bookmarkOn else bookmarkOff
+//            )
+//        }
     }
 
+    // 뒤로가기 버튼
     private fun initBackButton() {
         binding.ivBack.setOnClickListener {
             finish()
         }
-
-
     }
 }
