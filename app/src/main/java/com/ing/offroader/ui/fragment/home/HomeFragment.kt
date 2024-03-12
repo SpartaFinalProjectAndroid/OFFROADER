d0439c0469cda4eb788e11fb089317b3ccd1e4cc
rihan530 <renovatio530@gmail.com>
2024-03-12 17:32:54 +0900

    구글 소셜로그인 기능 추가

--- app/src/main/java/com/ing/offroader/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/ing/offroader/ui/fragment/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.ing.offroader.ui.fragment.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,7 +9,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ing.offroader.databinding.FragmentHomeBinding
+import com.ing.offroader.ui.activity.login.LoginActivity
+import com.ing.offroader.ui.activity.main.MainActivity
 import com.ing.offroader.ui.fragment.chatbot.MyApplication
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,15 +30,36 @@ class HomeFragment : Fragment() {
     private lateinit var  myPageAdapter: HomeMultiViewTypeAdapter
     private var uiData: List<HomeUiData> = listOf()
 
+    private var auth: FirebaseAuth ?= null
+    private val user = FirebaseAuth.getInstance().currentUser
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        if (user != null) {
+            binding.tvUsername.text = user.email ?: "츄파춥스님, 안녕하세요!"
+            Log.d(TAG, "onViewCreated: ${user.uid}")
+            Log.d(TAG, "onViewCreated: ${user.email}")
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initObserver()
+        binding.tvUsername.setOnClickListener {
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        if (user != null) {
+            binding.tvSignOut.visibility = View.VISIBLE
+        }
+        binding.tvSignOut.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun initObserver() {
