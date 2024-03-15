c0923e106cf70fb9023908c0b96da3c8e33f7be0
CodingVirus <twominyong@gmail.com>
2024-03-15 23:06:50 +0900

    feat : 유저 정보 파이어스토어에서 불러오기 구현중

--- app/src/main/java/com/ing/offroader/data/repository/UserRepository.kt
+++ app/src/main/java/com/ing/offroader/data/repository/UserRepository.kt
@@ -1,68 +1,123 @@
 package com.ing.offroader.data.repository
 
 import android.util.Log
+import androidx.room.util.copy
+import com.google.firebase.FirebaseException
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+import okhttp3.internal.wait
+import kotlin.system.measureTimeMillis
 
 
 class UserRepository {
 
-    private val TAG = "민용레포지토리"
+    private val TAG = "민용 레포지토리"
 
-    data class UserDataTest(
-        var userID: String ?= null,
-        var userLevel: Int ?= null,
-        var userName: String ?= null,
-        var achievements: Achievements = Achievements()
+    data class UserData(
+        var userName : String ?= null,
+        var userEmail : String ?= null,
+        var userAge : String ?= null,
+        var achievements : Achievements ?= null,
+        var community : Post ?= null,
     )
 
     data class Achievements(
+        var sanId: SanID ?= null,
         var attendance: Attendance ?= null,
-        var san: AchieveSanList ?= null
+    )
+
+    data class SanID(
+        var climb : Int?,
+        var distance : Int?,
+        var badgeTotal : Map<*, *>?
     )
 
     data class Attendance(
-        var total: Int,
+        var attendanceRecord : MutableList<*>?,
     )
 
-    data class AchieveSanList(
-        var sanList: MutableList<SanMap>
+    data class Post(
+        var title : String?,
+        var postId : String?,
+        var san : String?,
+        var uploadDate : String?,
+        var contents : String?,
+        var images : MutableList<*>?,
+        var like : MutableList<*>?
     )
 
-    data class SanMap(
-        var sanMap: Map<String, Int>
-    )
 
-    fun getUserData(userID: String) : UserDataTest {
+    suspend fun getUserData(userUID: String) : UserData {
 
-        var userTest  = UserDataTest()
+        val userDataClass = UserData()
 
-        var test = FirebaseFirestore.getInstance().collection("User").document(userID)
+        val userData = FirebaseFirestore.getInstance().collection("User").document(userUID)
 
-        test.get().addOnSuccessListener {
+        val userAchievements = userData.collection("Achievements")
+        val sanID = userAchievements.document("san_id").get()
+        val attendance = userAchievements.document("attendance ").get()
 
-            userTest.userID = it.id
-            userTest.userLevel = it.data?.get("level").toString().toInt()
-            userTest.userName = it.data?.get("name").toString()
+        val userCommunity = userData.collection("Community")
+        val post = userCommunity.document("post").get()
 
-            test.collection("achievements").document("attendance").get()
-                .addOnSuccessListener { it2 ->
-                    userTest.achievements.attendance =
-                        Attendance(it2.data?.get("total").toString().toInt())
-                    //userTest.achievements?.san?.sanList = SanMap(it2.data.get(""))
-                    Log.d(TAG, "getUserData: " + userTest.achievements.attendance)
-                }
 
-            test.collection("achievements").document("san").get()
-                .addOnSuccessListener { it3 ->
-                    //userTest.achievements.attendance = Attendance(it2.data?.get("total").toString().toInt())
-                    //userTest.achievements?.san?.sanList = SanMap(it2.data.get(""))
+        userData.get().addOnSuccessListener {
 
-                    Log.d(TAG, "getUserData: " + it3.data)
+            userDataClass.userName = it.get("user_name").toString()
+            userDataClass.userEmail = it.get("user_email").toString()
+            userDataClass.userAge = it.get("user_age").toString()
 
-                    // climb, distance
-                }
+            //Log.d(TAG, "name: $testName")
+        }.addOnFailureListener {
+            //Log.d(TAG, "getUserData: Fail")
+        }.await()
+
+
+
+
+        sanID.addOnSuccessListener {
+            //it.get("climb").toString().toInt()
+            userDataClass.achievements?.sanId?.climb = it.get("climb").toString().toInt()
+            it.get("distance").toString().toInt()
+            it.get("badge_total") as Map<*, *>
+
+
+        }.addOnFailureListener {
+
+        }.await()
+
+
+        attendance.addOnSuccessListener {
+            //val test2 = Attendance(it.get("attendance_record_2024") as MutableList<*>)
+            //attendanceCopy = test2.copy()
+            //Log.d(TAG, "getUserData: " + userDataClass.achievements?.attendance?.attendanceRecord)
         }
-        return userTest
-    }
+        post.addOnSuccessListener {
 
+            val test3 = Post(
+                it.get("title").toString(),
+                it.get("post_id").toString(),
+                it.get("san").toString(),
+                it.get("upload_date").toString(),
+                it.get("contents").toString(),
+                it.get("images") as MutableList<*>,
+                it.get("like") as MutableList<*>,
+            )
+
+
+            //Log.d(TAG, "getUserData: " + userDataClass.community)
+        }.await()
+
+        Thread.sleep(1000)
+        Log.d(TAG, "name: " + userDataClass.achievements?.sanId?.climb)
+
+        return userDataClass
+    }
 }
--- app/src/main/java/com/ing/offroader/ui/fragment/mydetail/MyDetailViewModel.kt
+++ app/src/main/java/com/ing/offroader/ui/fragment/mydetail/MyDetailViewModel.kt
@@ -1,10 +1,14 @@
 package com.ing.offroader.ui.fragment.mydetail
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.ing.offroader.data.repository.UserRepository
+import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class MyDetailViewModel : ViewModel() {
     private var _myDetailUiState = MutableLiveData<MyDetailUiState>()
@@ -24,7 +28,14 @@ class MyDetailViewModel : ViewModel() {
 
     private val userRepository : UserRepository = UserRepository()
 
-    fun getUserData(userID: String) {
-        userRepository.getUserData(userID = userID)
+    fun getUserData(userUID: String) {
+        var test: UserRepository.UserData ?= null
+        viewModelScope.launch {
+            val timeCheck = measureTimeMillis {
+                test = userRepository.getUserData(userUID = userUID)
+            }
+            //Log.d("민용뷰모델", "name: " + test?.achievements?.sanId?.climb)
+            Log.d("민용타임", "time: $timeCheck")
+        }
     }
 }
