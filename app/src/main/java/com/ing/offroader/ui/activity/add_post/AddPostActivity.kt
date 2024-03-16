7eee4b4e36ef17594db99fb472310e8f8de2638f
Euny Kim <euny4535@gmail.com>
2024-03-16 11:46:04 +0900

    layout : 게시글 올리는 화면 레이아웃 구성 완료

--- app/src/main/java/com/ing/offroader/ui/activity/add_post/AddPostActivity.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/add_post/AddPostActivity.kt
@@ -1,13 +1,9 @@
 package com.ing.offroader.ui.activity.add_post
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.ing.offroader.R
 import com.ing.offroader.databinding.ActivityAddPostBinding
-import com.ing.offroader.databinding.ActivityChatbotBinding
+
 private const val TAG = "AddPostActivity"
 class AddPostActivity : AppCompatActivity() {
 
