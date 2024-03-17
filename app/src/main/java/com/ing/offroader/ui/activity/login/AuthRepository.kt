9191c8d591f0fbfe4d2f13c1d8674ede5737572b
Euny Kim <euny4535@gmail.com>
2024-03-17 23:01:06 +0900

    add : 구글 로그인 mvvm으로 바꾸기 위해서 구조를 바꿔줌!

--- app/src/main/java/com/ing/offroader/ui/activity/login/AuthRepository.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/login/AuthRepository.kt
@@ -1,4 +1,51 @@
 package com.ing.offroader.ui.activity.login
 
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+
+
 class AuthRepository {
+
+    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val rootRef: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val usersRef: CollectionReference = rootRef.collection("user")
+
+    // Sign in using google
+    fun firebaseSignInWithGoogle(googleAuthCredential: AuthCredential): MutableLiveData<ResponseState<LoginUser>> {
+        val authenticatedUserMutableLiveData: MutableLiveData<ResponseState<LoginUser>> =
+            MutableLiveData()
+
+        firebaseAuth.signInWithCredential(googleAuthCredential).addOnCompleteListener { authTask ->
+            if (authTask.isSuccessful) {
+                var isNewUser = authTask.result?.additionalUserInfo?.isNewUser
+                val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
+                if (firebaseUser != null) {
+                    val uid = firebaseUser.uid
+                    val name = firebaseUser.displayName
+                    val email = firebaseUser.email
+                    val user = LoginUser(uid = uid, name = name, email = email)
+                    user.isNew = isNewUser
+                    authenticatedUserMutableLiveData.value = ResponseState.Success(user)
+
+                }
+
+
+            } else {
+
+                authenticatedUserMutableLiveData.value = authTask.exception?.message?.let {
+                    ResponseState.Error(it)
+                }
+
+            }
+
+
+        }
+        return authenticatedUserMutableLiveData
+    }
+
 }
--- app/src/main/java/com/ing/offroader/ui/activity/login/LoginViewModel.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/login/LoginViewModel.kt
@@ -1,4 +1,20 @@
 package com.ing.offroader.ui.activity.login
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.firestore.auth.User
+
+class LoginViewModel constructor(private val authRepository: AuthRepository) :
+    ViewModel() {
+
+    private var _authenticateUserLiveData: MutableLiveData<ResponseState<User>> = MutableLiveData()
+    val authenticateUserLiveData: LiveData<ResponseState<User>> get() = _authenticateUserLiveData
+
+    fun signInWithGoogle(googleAuthCredential: AuthCredential) {
+        _authenticateUserLiveData = authRepository.firebaseSignInWithGoogle(googleAuthCredential)
+    }
+
+
 }
