33070caaa360b6c79dda63a3f8144ab69b11667e
Euny Kim <euny4535@gmail.com>
2024-03-18 11:03:54 +0900

    fix : 구글 로그인 문제 해결함. sealed 클래스를 when문으로 사용할 때 오류가 있었나봄.!

--- app/src/main/java/com/ing/offroader/ui/activity/login/LoginViewModel.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/login/LoginViewModel.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.AuthCredential
-import com.google.firebase.firestore.auth.User
 
 class LoginViewModel constructor(private val authRepository: AuthRepository) :
     ViewModel() {
 
-    private var _authenticateUserLiveData: MutableLiveData<ResponseState<User>> = MutableLiveData()
-    val authenticateUserLiveData: LiveData<ResponseState<User>> get() = _authenticateUserLiveData
+    private var _authenticateUserLiveData: MutableLiveData<ResponseState<LoginUser>> =
+        MutableLiveData()
+    val authenticateUserLiveData: LiveData<ResponseState<LoginUser>> get() = _authenticateUserLiveData
 
     fun signInWithGoogle(googleAuthCredential: AuthCredential) {
         _authenticateUserLiveData = authRepository.firebaseSignInWithGoogle(googleAuthCredential)
