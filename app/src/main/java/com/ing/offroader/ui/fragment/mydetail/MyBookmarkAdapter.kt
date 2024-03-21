762901a65b8824dafcb06909eb5d20cd10013c43
kiwon kim <rlarldnjs17@gmail.com>
2024-03-21 14:06:20 +0900

    fix : appbar가 펼쳐진 상태에서 좋아요 버튼 안 먹던 오류 수정

--- app/src/main/java/com/ing/offroader/ui/fragment/mydetail/MyBookmarkAdapter.kt
+++ app/src/main/java/com/ing/offroader/ui/fragment/mydetail/MyBookmarkAdapter.kt
@@ -1,54 +1,45 @@
 package com.ing.offroader.ui.fragment.mydetail
 
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.ing.offroader.data.liked.LikedUtil
-import com.ing.offroader.databinding.ItemSanListBinding
-import com.ing.offroader.ui.activity.sandetail.SanDetailDTO
-
 interface OnBookmarkClickedInMyLikedListener{
     fun onBookmarkClicked()
 }
-class MyBookmarkAdapter(val mItems: MutableList<MyDetailUiState>) : RecyclerView.Adapter<MyBookmarkAdapter.Holder>(), OnBookmarkClickedInMyLikedListener {
-
-    var onBookmarkClickedInMyLikedListener: List<OnBookmarkClickedInMyLikedListener>? = null
-    private var bookmarkedItem = LikedUtil.getLiked()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding = ItemSanListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return Holder(binding)
-    }
-
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(bookmarkedItem[position])
-    }
-
-    override fun getItemCount(): Int {
-        return bookmarkedItem.size
-    }
-
-    fun refreshRecyclerView() {
-        bookmarkedItem = LikedUtil.getLiked()
-        notifyDataSetChanged()
-    }
-
-    override fun onBookmarkClicked() {
-        Log.d("MyBookmarkAdapter", "onBookmarkClickedInMyLiked")
-        refreshRecyclerView()
-    }
-
-    inner class Holder(val binding: ItemSanListBinding) : RecyclerView.ViewHolder(binding.root) {
-        var mountainImage = binding.ivSanImage
-        var mountainName = binding.tvSanName
-
-        fun bind(item: SanDetailDTO) {
-            mountainName.text = item.mountain
-
-            Glide.with(mountainImage.context)
-                .load(mItems)
-                .into(mountainImage)
-        }
-    }
-}
+//class MyBookmarkAdapter(val mItems: MutableList<MyDetailUiState>) : RecyclerView.Adapter<MyBookmarkAdapter.Holder>(), OnBookmarkClickedInMyLikedListener {
+//
+//    var onBookmarkClickedInMyLikedListener: List<OnBookmarkClickedInMyLikedListener>? = null
+//    private var bookmarkedItem = LikedUtil.getLiked()
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+//        val binding = ItemSanListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return Holder(binding)
+//    }
+//
+////    override fun onBindViewHolder(holder: Holder, position: Int) {
+////        holder.bind(bookmarkedItem[position])
+////    }
+////
+////    override fun getItemCount(): Int {
+////        return bookmarkedItem.size
+////    }
+//
+//    fun refreshRecyclerView() {
+//        bookmarkedItem = LikedUtil.getLiked()
+//        notifyDataSetChanged()
+//    }
+//
+//    override fun onBookmarkClicked() {
+//        Log.d("MyBookmarkAdapter", "onBookmarkClickedInMyLiked")
+//        refreshRecyclerView()
+//    }
+//
+//    inner class Holder(val binding: ItemSanListBinding) : RecyclerView.ViewHolder(binding.root) {
+//        var mountainImage = binding.ivSanImage
+//        var mountainName = binding.tvSanName
+//
+//        fun bind(item: SanDetailDTO) {
+//            mountainName.text = item.mountain
+//
+//            Glide.with(mountainImage.context)
+//                .load(mItems)
+//                .into(mountainImage)
+//        }
+//    }
+//}
--- app/src/main/java/com/ing/offroader/ui/fragment/mydetail/MyDetailFragment.kt
+++ app/src/main/java/com/ing/offroader/ui/fragment/mydetail/MyDetailFragment.kt
@@ -1,73 +1,47 @@
 package com.ing.offroader.ui.fragment.mydetail
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
 import com.ing.offroader.databinding.FragmentMyDetailBinding
 import com.ing.offroader.ui.activity.achievement.AchievementActivity
-import com.ing.offroader.ui.activity.my_post.MyPostActivity
-import com.ing.offroader.ui.fragment.community.MyApplication
-import com.ing.offroader.ui.fragment.community.adapter.CommunityAdapter
-import com.ing.offroader.ui.fragment.community.viewmodel.CommunityViewModel
-import com.ing.offroader.ui.fragment.community.viewmodel.CommunityViewModelFactory
 
 class MyDetailFragment : Fragment() {
 
     companion object {
-        private const val TAG = "태그 : MyDetailFragment"
+        fun newInstance() = MyDetailFragment()
     }
 
     private var _binding: FragmentMyDetailBinding? = null
     private val binding get() = _binding!!
 
-
-
-    private lateinit var myBookmarkAdapter: MyBookmarkAdapter
+//    private lateinit var myBookmarkAdapter: MyBookmarkAdapter
 
     private val myDetailViewModel by viewModels<MyDetailViewModel>()
-    private val communityViewModel: CommunityViewModel by viewModels {
-        CommunityViewModelFactory((requireActivity().application as MyApplication).postRepository)
-    }
-    private val communityAdapter: CommunityAdapter by lazy {
-        CommunityAdapter(communityViewModel)
-    }
 
-    // 사용자 정보 가져오기
-    private var user = FirebaseAuth.getInstance().currentUser
 
-    private var myPosts : Boolean = false
-
-    @SuppressLint("InflateParams")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         _binding = FragmentMyDetailBinding.inflate(inflater, container, false)
 
         myDetailViewModel.getUserData("user_test") // 파이어스토에 해당 유저 UID에 맞는 데이터 가져오기
 
         return binding.root
 
-        /** 프래그먼트에는 onCreateView랑 onViewCreated 가 둘다 있는데 onCreateView에서는 바인딩해주는
-         *  작업만 해주고 모든 초기화및 함수 로직은 onViewCreated에서 해줘야한다고 튜터님께서 말씀해주셨어요
-         *  꼬일수도 있다고 하셨던 것 같습니다!
-         *
-         **/
+
+        initLikedRecyclerView()
 
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
 
         initBlur()
 
@@ -76,62 +50,6 @@ class MyDetailFragment : Fragment() {
 
         // 업적창으로 이동
         goToAchieveActivity()
-
-
-    }
-
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume: ")
-        initView()
-    }
-
-    private fun initView() {
-        Log.d(TAG, "initView: ")
-        setUpUserDetail()
-        setUpListeners()
-
-    }
-
-    private fun setUpUserDetail() {
-        user = FirebaseAuth.getInstance().currentUser
-        Log.d(TAG, "setUpUserDetail: ${user?.uid}")
-        when (user?.uid) {
-            null -> setNoLoggedInUser()
-            else -> setUserInformation()
-        }
-    }
-
-    private fun setUserInformation() = with(binding) {
-        tvLogin.visibility = View.INVISIBLE
-        tvId.visibility= View.INVISIBLE
-        tvName.visibility= View.VISIBLE
-        tvName.text = user?.displayName
-        tvNameNim.visibility= View.VISIBLE
-        // 아직 구현이 안된 부분이라 숨겨둘 예정
-        // 회원 가입 시 로그인 정부 추가 하면 구현할 VISIBLE로 바꿔주고 적절한 값을 추가해주면 되지 않울까욤.
-        tvProfilInfo.visibility= View.INVISIBLE
-        clAddress.visibility = View.INVISIBLE
-        Glide.with(requireActivity()).load(user?.photoUrl).into(ivProfil)
-
-
-    }
-
-    private fun setNoLoggedInUser() = with(binding) {
-        tvLogin.visibility = View.VISIBLE
-
-        tvId.visibility=View.VISIBLE
-        tvName.visibility = View.INVISIBLE
-        tvNameNim.visibility= View.INVISIBLE
-        tvProfilInfo.visibility= View.INVISIBLE
-    }
-
-    private fun setUpListeners() = with(binding) {
-        clMyPost.setOnClickListener {
-            val intent = Intent(requireActivity(), MyPostActivity::class.java)
-            startActivity(intent)
-        }
 
     }
 
@@ -164,18 +82,16 @@ class MyDetailFragment : Fragment() {
 
     // 톱니바퀴 누르면 setting
     private fun goToSettingFragment() {
-        binding.ivSetting.setOnClickListener { }
+        binding.ivSetting.setOnClickListener {  }
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
 
     private fun initLikedRecyclerView() {
-        myBookmarkAdapter.onBookmarkClickedInMyLikedListener = listOf()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-
+//        myBookmarkAdapter.onBookmarkClickedInMyLikedListener = listOf()
     }
 
 }
