a56bf7eadc89ae118ef9e80d558d2f7994856c37
Box-o <jeffmailkr@gmail.com>
2024-03-21 23:41:14 +0900

    feat : 커뮤니티 포스팅 액티비티에 이미지 추가 구현

--- app/src/main/java/com/ing/offroader/ui/activity/add_post/AddPostActivity.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/add_post/AddPostActivity.kt
@@ -1,26 +1,33 @@
 package com.ing.offroader.ui.activity.add_post
 
+import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.github.dhaval2404.imagepicker.ImagePicker
 import com.ing.offroader.databinding.ActivityAddPostBinding
+import org.apache.commons.io.output.ByteArrayOutputStream
 
 private const val TAG = "AddPostActivity"
+
 class AddPostActivity : AppCompatActivity() {
     private var _binding: ActivityAddPostBinding? = null
     private val binding get() = _binding!!
-
     private val addPostViewModel by viewModels<AddPostViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityAddPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initView()
         initObserver()
-
     }
 
     private fun initObserver() {
@@ -32,7 +39,7 @@ class AddPostActivity : AppCompatActivity() {
                     Log.d(TAG, "initObserver: finish")
                     finish()
                 } else {
-                    Toast.makeText(this,it.errorMessage,Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, it.errorMessage, Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -42,17 +49,53 @@ class AddPostActivity : AppCompatActivity() {
         setupListener()
     }
 
-    private fun setupListener() = with(binding){
-        etTitle.addTextChangedListener {
-            addPostViewModel.titleChangedListener(etTitle.text.toString())
-        }
-        etContent.addTextChangedListener {
-            addPostViewModel.contentChangedListener(etContent.text.toString())
-        }
-        tvComplete.setOnClickListener {
-            addPostViewModel.setOnCompleteButton()
-        }
-        // TODO : 1. 갤러리에서 이미지 가져오는 코드 짜기
+    private fun setupListener() = with(binding) {
+        etTitle.addTextChangedListener { addPostViewModel.titleChangedListener(etTitle.text.toString()) }
+        etContent.addTextChangedListener { addPostViewModel.contentChangedListener(etContent.text.toString()) }
+        tvComplete.setOnClickListener { addPostViewModel.setOnCompleteButton() }
+        ivAddImage.setOnClickListener { imgPicker() }
         // TODO : 2. 카메라로 이동해서 사진 찍는 코드
     }
+
+    //이미지 픽커 라이브러리를 사용해 간단하게 편집할 수 있고, 사진의 최대 크기와 용량을 제한함
+    private fun imgPicker() {
+        ImagePicker.with(this)
+            .galleryOnly() //갤러리에서만 사진을 불러올 수 있도록 지정
+            .compress(1024) //일메가로압축
+            .maxResultSize(1080, 1080) //또는 이미지크기 가세로 1080 제한
+            .cropSquare() //사진의 비율을 1:1로 지정
+            .createIntent { intent -> imageResult.launch(intent) } //class 처음에 작성한 이미지 선택 Intent 실행
+    }
+
+    //가져온 사진 uri를 이미지로 변경하고 byteArray로 사진을 전달
+    private val imageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                //정상적으로 이미지를 가져온 경우
+                Activity.RESULT_OK -> {
+                    val fileUri = data?.data!!
+
+                    //이미지를 포스트 액티비티의 사진 부분에 적용하고
+                    binding.ivSelectedImg.setImageURI(fileUri)
+
+                    //가져온 사진 uri를 이미지로 변경해
+                    val bitmap = (binding.ivSelectedImg.drawable as BitmapDrawable).bitmap
+                    val baos = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                    val data = baos.toByteArray()
+
+                    //addPostViewModel를 통해 Firebase에 전달
+                    addPostViewModel.addImageUri(data)
+                }
+
+                //에러가 발생한 경우 어떤 에러인지 토스트로 알림
+                ImagePicker.RESULT_ERROR -> { Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show() }
+
+                //선택을 취소한 경우 토스트로 알림
+                else -> { Toast.makeText(this, "선택을 취소하셨습니다.", Toast.LENGTH_SHORT).show() }
+            }
+        }
 }
