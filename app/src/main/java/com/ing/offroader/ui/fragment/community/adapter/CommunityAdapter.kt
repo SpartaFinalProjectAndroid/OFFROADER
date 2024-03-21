53a9bb46cd812e78b209bef28b2193a4d9944372
Euny Kim <euny4535@gmail.com>
2024-03-22 05:07:28 +0900

    feat : 파이어스토어에 있는 이미지를 메모리에 저장하는 방법으로 불러와서 바인딩해줌. 잘 적용됨.

--- app/src/main/java/com/ing/offroader/ui/fragment/community/adapter/CommunityAdapter.kt
+++ app/src/main/java/com/ing/offroader/ui/fragment/community/adapter/CommunityAdapter.kt
@@ -1,5 +1,6 @@
 package com.ing.offroader.ui.fragment.community.adapter
 
+import android.graphics.BitmapFactory
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.ing.offroader.databinding.ItemPostBinding
 import com.ing.offroader.ui.fragment.community.model.PostDTO
 import com.ing.offroader.ui.fragment.community.viewmodel.CommunityViewModel
@@ -29,6 +32,25 @@ class CommunityAdapter(private val viewModel: CommunityViewModel) :
         val item = getItem(position)
 
         (holder as PostItemViewHolder).apply {
+
+
+            val storage = Firebase.storage("gs://offroader-event.appspot.com")
+            // 저장소 위치 참조
+            val storageRef = storage.reference
+            // 파일 위치 참조
+            val pathRef = storageRef.child("Offroader_res/post_image/${item.post_id}.jpg")
+
+            // 디비 스토리지에서 받아온 값을 메모리에 저장하려고 함. 앱 메모리보다 큰 사진을 불러오면 크래시가 나기 때문에 불러올 때 메모리의 크기 제한을 둠.
+            val ONE_MEGABYTE: Long = 1024*1024
+            pathRef.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+                // 바이트어레이를 비트맵으로 변환해주는 코드
+                val image = BitmapFactory.decodeByteArray(it,0,it.size)
+                // 비트맵을 바인딩해주는 코드
+                postImage.setImageBitmap(image)
+            }.addOnFailureListener {
+                Log.d(TAG, "onBindViewHolder: 사진 받아오는거 실패함. 알아서 하셈.")
+            }
+
 
             userid.text = user!!.email
             title.text = item.title.toString()
