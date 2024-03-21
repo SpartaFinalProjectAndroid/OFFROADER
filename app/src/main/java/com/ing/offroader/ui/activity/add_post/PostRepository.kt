f33f37b9c870b2eaa9f461142b7bd7c783ee8cab
Euny Kim <euny4535@gmail.com>
2024-03-22 04:24:30 +0900

    comment : 코드 정리

--- app/src/main/java/com/ing/offroader/ui/activity/add_post/PostRepository.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/add_post/PostRepository.kt
@@ -36,21 +36,25 @@ class PostRepository {
     fun setPost() {
         Log.d(TAG, "setPost: ")
 
-        var postItems: ArrayList<PostDTO?> = arrayListOf()
-        db.collection("Community").orderBy("upload_date", Query.Direction.DESCENDING).get().addOnSuccessListener { it ->
-            it.documents.forEach {
-                val post = it.toObject(PostDTO::class.java)
-                postItems.add(post)
-            }
-            Log.d(
-                TAG,
-                "setPosts: postItems : $postItems"
-            )
-            //                updateUiState
+        try {
+            var postItems: ArrayList<PostDTO?> = arrayListOf()
+            db.collection("Community").orderBy("upload_date", Query.Direction.DESCENDING).get().addOnSuccessListener { it ->
+                it.documents.forEach {
+                    val post = it.toObject(PostDTO::class.java)
+                    postItems.add(post)
+                }
+                Log.d(
+                    TAG,
+                    "setPosts: postItems : $postItems"
+                )
+                //                updateUiState
 
-            val items = postItems
-            _setPostItems.value = items
-            Log.d(TAG, "setPosts: ${setPostItems.value}")
+                val items = postItems
+                _setPostItems.value = items
+                Log.d(TAG, "setPosts: ${setPostItems.value}")
+            }
+        }catch (e: Exception) {
+            Log.e(TAG, "FireStore Error: $e")
         }
 
     }
@@ -110,61 +114,4 @@ class PostRepository {
         }
     }
 
-
-//    suspend fun setPost(): MutableList<DocumentSnapshot>? {
-//        Log.d(TAG, "setPost: ")
-//        return try {
-//            db.collection("Community").orderBy("upload_date").get().await().documents
-//
-//        } catch (e: Exception) {
-//            Log.e(TAG, "FireStore Error: $e")
-//            null
-//        }
-//    }
-//
-//
-//    suspend fun addPost(title: String, content: String?, image: ByteArray?) : Boolean {
-//        Log.d(TAG, "addPost uid: ${user?.uid}")
-//
-//
-//        val postId = UUID.randomUUID().toString()
-//
-//        // 이미지를 저장할 위치를 지정
-//        val storageRef =
-//            specifiedStorage.reference.child("Offroader_res/post_image/${postId}.jpg")
-//
-//        //putBytes 함수를 사용해 전달받은 이미지를 firebase에 업로드
-//        val uploadTask = storageRef.putBytes(image!!)
-//        //성공,실패 여부 확인 로그
-//        uploadTask.addOnFailureListener {
-//            Log.d(TAG, "테스트: 실패")
-//        }.addOnSuccessListener { taskSnapshot ->
-//            Log.d(TAG, "테스트: 성공")
-//        }
-//
-//        val date = LocalDateTime.now()
-//        val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
-//        val dateToLong = date.format(formatter).toLong()
-//
-//        Log.d(TAG, "addPost: $dateToLong")
-//
-//        val newPost = hashMapOf(
-//            "uid" to user!!.uid,
-//            "title" to title,
-//            "contents" to content,
-//            "like" to 0,
-//            "post_id" to postId,
-//            "san" to null,
-//            "upload_date" to dateToLong
-//        )
-//
-//        return try {
-//            db.collection("Community").document(postId).set(newPost).await()
-//            true
-//
-//        } catch (e: Exception) {
-//            Log.e(TAG, "FireStore Error: $e")
-//            false
-//        }
-//    }
 }
