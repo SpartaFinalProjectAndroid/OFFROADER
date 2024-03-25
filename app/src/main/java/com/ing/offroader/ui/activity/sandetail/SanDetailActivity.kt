009a6e3a7028ce4237976e8b2655dd44a6839bf6
kiwon kim <rlarldnjs17@gmail.com>
2024-03-25 21:17:23 +0900

    fix : 자동스크롤 속도 급격하게 빨라지는 오류 수정

--- app/src/main/java/com/ing/offroader/ui/activity/sandetail/SanDetailActivity.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/sandetail/SanDetailActivity.kt
@@ -149,8 +149,8 @@ class SanDetailActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListe
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 // 이미지 사진이 1장일 때 자동 스크롤 방지
+                slideImageHandler.removeCallbacks(slideImageRunnable)
                 if (imageSize > 2) { slideImageHandler.postDelayed(slideImageRunnable, 5000) }
-                else { slideImageHandler.removeCallbacks(slideImageRunnable) }
             }
         })
     }
