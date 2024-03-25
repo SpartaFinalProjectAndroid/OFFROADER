53181f587b8f3713d1618a75172dbf036a71fc2c
Euny Kim <euny4535@gmail.com>
2024-03-25 16:33:47 +0900

    refactor : 이미지 당 메모리 최대 량 4메가로 올려 봄.

--- app/src/main/java/com/ing/offroader/ui/activity/add_post/PostRepository.kt
+++ app/src/main/java/com/ing/offroader/ui/activity/add_post/PostRepository.kt
@@ -46,9 +46,9 @@ class PostRepository {
         val pathRef = storageRef.child("Offroader_res/post_image/${post?.post_id}.jpg")
 
         // 디비 스토리지에서 받아온 값을 메모리에 저장하려고 함. 앱 메모리보다 큰 사진을 불러오면 크래시가 나기 때문에 불러올 때 메모리의 크기 제한을 둠.
-        val ONE_MEGABYTE: Long = 1024 * 1024
+        val FOUR_MEGABYTE: Long = 1024 * 1024 * 4
 
-        pathRef.getBytes(ONE_MEGABYTE).addOnSuccessListener { it ->
+        pathRef.getBytes(FOUR_MEGABYTE).addOnSuccessListener { it ->
             // 바이트어레이를 비트맵으로 변환해주는 코드
 
             val image = BitmapFactory.decodeByteArray(it, 0, it.size)
